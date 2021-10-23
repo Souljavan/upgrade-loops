0253ab5718dc21c78b4a2339f06cf28910b07ecc
souljavan <jcavanilles@gmail.com>
2021-10-23 12:34:25 +0200

    iteracion1

---
+++ Iteracion.js
@@ -0,0 +1,6 @@
+const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta'];
+for (prod of products){
+    if (prod.includes("Camiseta")){
+       console.log(prod)
+    }
+}
